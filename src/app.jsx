de531f56a357e1eb6dbd371acdb9dbbfc3e77e32
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-02 21:07:12 +0500

    use state hook

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassCounter from "./components/class-counter";
+import HookCounter from "./components/hook-counter";
+
+function App() {
+  return (
+    <>
+      {/* <ClassCounter /> */}
+      <HookCounter />
+    </>
+  );
+}
+
+export default App;
