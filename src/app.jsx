002de9163da67356b1dc9b5f6886c488946d7a04
Abdulxoliq <odiljonovabdulxoliq11@gmail.com>
2022-08-21 12:10:28 +0500

    using reducer fetching data

--- src/app.jsx
+++ src/app.jsx
@@ -1,36 +1,13 @@
-import React, { useReducer } from "react";
-import CompA from "./components/comp-a";
-import CompB from "./components/comp-b";
-import CompC from "./components/comp-c";
-
-export const CountContext = React.createContext();
-
-const initialState = 0;
-const reducer = (state, action) => {
-  switch (action) {
-    case "increment":
-      return state + 1;
-    case "decrement":
-      return state - 1;
-    case "reset":
-      return initialState;
-
-    default:
-      return state;
-  }
-};
+import React from "react";
+import DataFetchingOne from "./components/data-fetching-one";
+import DataFetchingTwo from "./components/data-fetching-two";
 
 function App() {
-  const [count, dispatch] = useReducer(reducer, initialState);
   return (
-    <CountContext.Provider
-      value={{ countState: count, countDispatch: dispatch }}
-    >
-      <h1>Count - {count}</h1>
-      <CompA />
-      <CompB />
-      <CompC />
-    </CountContext.Provider>
+    <>
+      {/* <DataFetchingOne /> */}
+      <DataFetchingTwo />
+    </>
   );
 }
 
